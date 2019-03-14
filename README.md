b6155cadd13b99792edef070de4ffe221d1d95e9
dujiawei <jiawei.du@goxueche.com>
2019-03-14 11:55:49 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,166 @@
+## 时间段函数集合
+
+调用方法，返回特定的时间段，适用于动态获取日期。
+
+### API
+
+#### 获取当天的日期
+
+today
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {string} YYYY-MM-DD
+
+```js
+import { today } from 'period-date';
+
+console.log(today()); // 2019-03-13
+```
+
+#### 获取昨天的日期
+
+yesterday
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {string} YYYY-MM-DD
+
+```js
+import { yesterday } from 'period-date';
+
+console.log(yesterday()); // 2019-03-12
+```
+
+#### 获取明天的日期
+
+tomorrow
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {string} YYYY-MM-DD
+
+```js
+import { tomorrow } from 'period-date';
+
+console.log(tomorrow()); // 2019-03-14
+```
+
+#### 获取指定月 1号，默认返回 本月1号
+
+monthFirstDay
+
+* @param {number || string} year   年 默认本年
+* @param {number || string} month  月 默认本月
+* @param {string} type  分隔符 默认 '-'
+* @returns {string} YYYY-MM-DD
+
+```js
+import { monthFirstDay } from 'period-date';
+
+console.log(monthFirstDay()); // 2019-03-01
+console.log(monthFirstDay({ year: 2018, month: 9 })); // 2018-09-01
+2019-03-31
+```
+
+#### 获取指定月最后一天的日期，默认返回 本月最后一天
+
+monthLastDay
+
+* @param {number || string} year   年 默认本年
+* @param {number || string} month  月 默认本月
+* @param {string} type  分隔符 默认 '-'
+* @returns {string} YYYY-MM-DD
+
+```js
+import { monthLastDay } from 'period-date';
+
+console.log(monthLastDay()); // 2019-03-31
+console.log(monthLastDay({ year: 2018, month: 9 })); // 2018-09-30
+```
+
+#### 上个月时间段，默认返回当前月的 上个月
+
+prevMonth
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+
+```js
+import { prevMonth } from 'period-date';
+
+console.log(prevMonth()); // ["2019-02-01", "2019-02-28"]
+console.log(prevMonth({ year: 2018, month: 9 })); // ["2018-08-01", "2018-08-31"]
+```
+
+#### 下个月时间段，默认返回当前月的 下个月
+
+nextMonth
+
+* @param {string} [type='-'] 分隔符 默认 '-'
+* @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+
+```js
+import { nextMonth } from 'period-date';
+
+console.log(nextMonth()); // ["2019-04-01", "2019-04-30"]
+console.log(nextMonth({ year: 2018, month: 9 })); // ["2018-10-01", "2018-10-31"]
+```
+
+#### 返回指定月的 1 号是周几，这个月有多少天，默认返回当前月
+
+monthInfo
+
+* @param {number} year   年 默认本年
+* @param {number} month  月 默认本月
+* @returns {object} date:日期、firstDay:1号是周几、days:该月的天数
+
+```js
+import { monthInfo } from 'period-date';
+
+console.log(monthInfo()); // {date: "2019-03", firstDay: 5, days: 31}
+console.log(monthInfo({ year: 2018, month: 9 })); // {date: "2018-09", firstDay: 6, days: 30}
+```
+
+#### 输入日期，返回指定日期所在的一周的日期时间段，从周一到周日，默认是本周
+
+week
+
+* @param {number} year   年 默认本年
+* @param {number} month  月 默认本月
+* @param {number} day    日 默认今天
+* @param {string} type  分隔符 默认 '-'
+* @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+
+```js
+import { monthInfo } from 'period-date';
+
+console.log(week()); // ["2019-03-11", "2019-03-17"]
+console.log(week({ year: 2018, month: 9, day: 10 })); // ["2018-09-10", "2018-09-16"]
+```
+
+#### 获取指定日期 的 上周的日期时间段，默认返回 当前周的上周
+
+prevWeek
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+
+```js
+import { prevWeek } from 'period-date';
+
+console.log(prevWeek()); // ["2019-03-04", "2019-03-10"]
+console.log(prevWeek({ year: 2018, month: 9, day: 10 })); // ["2018-09-03", "2018-09-09"]
+```
+
+#### 获取指定日期 的 下周的时间段，默认返回 当前周的下周
+
+nextWeek
+
+* @param {string} [type="-"] 分隔符 默认 '-'
+* @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+
+```js
+import { nextWeek } from 'period-date';
+
+console.log(nextWeek()); // ["2019-03-18", "2019-03-24"]
+console.log(nextWeek({ year: 2018, month: 9, day: 10 })); // ["2018-09-17", "2018-09-23"]
+```
+
---
+++ index.js
@@ -0,0 +1,233 @@
+/*
+ * @作者: jiawei 
+ * @创建时间: 2019-03-13 11:38:15 
+ * @文件主题: 时间段函数
+ *
+ */
+
+/**
+ * 判断小于 10，补 0
+ *
+ * @param {*} x
+ * @returns
+ */
+function datePlus0(x) {
+    if (x < 10) {
+        return `0${x}`;
+    } else {
+        return x;
+    }
+}
+
+
+/**
+ * 当前日期
+ *
+ * @returns {year, month, day}
+ */
+function nowDate() {
+    return {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth(),
+        day: new Date().getDate()
+    }
+}
+
+
+/**
+ * 优化日期格式
+ *
+ * @param {Object} Date Object
+ * @returns {year, month, day}
+ */
+function optimize(date) {
+    return {
+        year: date.getFullYear(),
+        month: datePlus0(date.getMonth() + 1),
+        day: datePlus0(date.getDate())
+    }
+}
+
+
+/**
+ * 今天的日期
+ *
+ * @param {string} [type="-"] 分隔符 默认 '-'
+ * @returns {string} YYYY-MM-DD
+ */
+function today(type = "-") {
+    let { year, month, day } = optimize(new Date());
+    return `${year}${type}${month}${type}${day}`;
+}
+
+
+/**
+ * 昨天的日期
+ *
+ * @param {string} [type='-'] 分隔符 默认 '-'
+ * @returns {string} YYYY-MM-DD
+ */
+function yesterday(type = '-') {
+    let { year, month, day } = optimize(new Date(nowDate().year, nowDate().month, nowDate().day - 1));
+    return `${year}${type}${month}${type}${day}`;
+}
+
+
+/**
+ * 明天的日期
+ *
+ * @param {string} [type='-'] 分隔符 默认 '-'
+ * @returns {string} YYYY-MM-DD
+ */
+function tomorrow(type = '-') {
+    let { year, month, day } = optimize(new Date(nowDate().year, nowDate().month, nowDate().day + 1));
+    return `${year}${type}${month}${type}${day}`;
+}
+
+
+/**
+ * 获取指定月 1号
+ * 默认返回 本月1号
+ *
+ * @param {number} year   年 默认本年
+ * @param {number} month  月 默认本月
+ * @param {string} type  分隔符 默认 '-'
+ * @returns {string} YYYY-MM-DD
+ */
+function monthFirstDay({ year = nowDate().year, month = nowDate().month + 1, type = "-" } = {}) {
+    let { year: _year, month: _month, day: _day } = optimize(new Date(year, Number(month) - 1, 1));
+    return `${_year}${type}${_month}${type}${_day}`;
+}
+
+
+/**
+ * 获取指定月最后一天的日期
+ * 默认返回 本月最后一天
+ *
+ * @param {number} year   年 默认本年
+ * @param {number} month  月 默认本月
+ * @param {string} type  分隔符 默认 '-'
+ * @returns {string} YYYY-MM-DD
+ */
+function monthLastDay({ year = nowDate().year, month = nowDate().month + 1, type = "-" } = {}) {
+    let { year: _year, month: _month, day: _day } = optimize(new Date(year, month, 0));
+    return `${_year}${type}${_month}${type}${_day}`;
+}
+
+
+/**
+ * 上个月时间段
+ * 默认返回当前月的 上个月
+ *
+ * @param {string} [type="-"] 分隔符 默认 '-'
+ * @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+ */
+function prevMonth({ year = nowDate().year, month = nowDate().month + 1, type = "-" } = {}) {
+    return [monthFirstDay({ year, month: Number(month) - 1, type }), monthLastDay({ year, month: Number(month) - 1, type })];
+}
+
+
+/**
+ * 下个月时间段
+ * 默认返回当前月的 下个月
+ *
+ * @param {string} [type='-'] 分隔符 默认 '-'
+ * @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+ */
+function nextMonth({ year = nowDate().year, month = nowDate().month + 1, type = "-" } = {}) {
+    return [monthFirstDay({ year, month: Number(month) + 1, type }), monthLastDay({ year, month: Number(month) + 1, type })];
+}
+
+
+/**
+ * 返回指定月的 1 号是周几，这个月有多少天
+ * 默认返回当前月
+ *
+ * @param {number} year   年 默认本年
+ * @param {number} month  月 默认本月
+ * @returns {object} date:日期、firstDay:1号是周几、days:该月的天数
+ */
+function monthInfo({ year = nowDate().year, month = nowDate().month + 1, type = "-" } = {}) {
+    // 1 号是周几
+    let date = new Date(year, Number(month) - 1, 1);
+    console.log(date);
+    let weekDay = date.getDay();
+    // 该月最后一天是几号
+    let days = new Date(year, month, 0).getDate();
+
+    // 0 是星期日，返回 7
+    weekDay = weekDay === 0 ? 7 : weekDay
+    console.log(date.getMonth());
+    return {
+        date: `${date.getFullYear()}${type}${datePlus0(date.getMonth() + 1)}`,
+        firstDay: weekDay,
+        days: days
+    }
+}
+
+
+/**
+ * 输入日期，返回指定日期所在的一周的日期时间段，从周一到周日
+ * 默认是本周
+ *
+ * @param {number} year   年 默认本年
+ * @param {number} month  月 默认本月
+ * @param {number} day    日 默认今天
+ * @param {string} type  分隔符 默认 '-'
+ * @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+ */
+function week({ year = nowDate().year, month = nowDate().month + 1, day = nowDate().day, type = "-" } = {}) {
+    // 获取到今天是周几，如果是 0 ，说明是周日
+    let weekDay = new Date(year, Number(month) - 1, day).getDay();
+    weekDay = weekDay === 0 ? 7 : weekDay
+
+    // 周一的日期是，当前日期的 day - weekDay + 1
+    let { year: week_start_year, month: week_start_month, day: week_start_day } = optimize(new Date(year, Number(month) - 1, day - weekDay + 1));
+    // 周日的日期是，当前日期的 day - weekDay + 7
+    let { year: week_end_year, month: week_end_month, day: week_end_day } = optimize(new Date(year, Number(month) - 1, day - weekDay + 7));
+
+    return [`${week_start_year}${type}${week_start_month}${type}${week_start_day}`, `${week_end_year}${type}${week_end_month}${type}${week_end_day}`];
+}
+
+
+/**
+ * 获取指定日期 的 上周的日期时间段
+ * 默认返回 当前周的上周
+ *
+ * @param {string} [type="-"] 分隔符 默认 '-'
+ * @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+ */
+function prevWeek({ year = nowDate().year, month = nowDate().month + 1, day = nowDate().day, type = "-" } = {}) {
+    let weekDay = new Date(year, Number(month) - 1, day).getDay();
+    weekDay = weekDay === 0 ? 7 : weekDay
+
+    return week({ year, month, day: day - weekDay, type });
+}
+
+
+/**
+ * 获取指定日期 的 下周的时间段
+ * 默认返回 当前周的下周
+ *
+ * @param {string} [type="-"] 分隔符 默认 '-'
+ * @returns {array} [YYYY-MM-DD, YYYY-MM-DD]
+ */
+function nextWeek({ year = nowDate().year, month = nowDate().month + 1, day = nowDate().day, type = "-" } = {}) {
+    let weekDay = new Date(year, Number(month) - 1, day).getDay();
+    weekDay = weekDay === 0 ? 7 : weekDay
+
+    return week({ year, month, day: day + 8 - weekDay, type });
+}
+
+
+exports.today = today;
+exports.yesterday = yesterday;
+exports.tomorrow = tomorrow;
+exports.monthFirstDay = monthFirstDay;
+exports.monthLastDay = monthLastDay;
+exports.prevMonth = prevMonth;
+exports.nextMonth = nextMonth;
+exports.monthInfo = monthInfo;
+exports.week = week;
+exports.prevWeek = prevWeek;
+exports.nextWeek = nextWeek;
